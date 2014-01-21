e618a650655d75132f580b1fdaf502463be7cfd3
rahwang <rah1@uchicago.edu>
2014-01-20 18:46:07 -0600

    testing

---
+++ ParallelComputing/project1/tests/serial_test.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <string.h>
+#include "CUnit/Basic.h"
+
+
+/* Pointer to the file used by the tests. */
+static FILE* temp_file = NULL;
+
+/* The suite initialization function.
+ * Opens the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite1(void)
+{
+   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
+      return -1;
+   }
+   else {
+      return 0;
+   }
+}
+
+/* The suite cleanup function.
+ * Closes the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite1(void)
+{
+   if (0 != fclose(temp_file)) {
+      return -1;
+   }
+   else {
+      temp_file = NULL;
+      return 0;
+   }
+}
+
+/* Generates a random nxn matrix */
+int *randomM(int n)
+{
+    int i, j;
+    int *new = (int *) malloc(n*n*sizeof(int));
+
+    srand(time(NULL));
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (i != j)
+                new[i*n+j] = (int) rand() % 20;
+            else
+                new[i*n+j] = 0;
+        }
+    }
+
+    return new;
+}
+
+
+
+/* Simple test of readm().
+ * Writes random matrix to file and checks whether read 
+ * matrix matches expected values
+ */
+void testREADM(void)
+{
+  int *m = randomM(10);
+
+   if (NULL != temp_file) {
+      CU_ASSERT(0 == fprintf(temp_file, ""));
+      CU_ASSERT(2 == fprintf(temp_file, "Q\n"));
+      CU_ASSERT(7 == fprintf(temp_file, "i1 = %d", i1));
+   }
+}
+
+/* Simple test of fread().
+ * Reads the data previously written by testFPRINTF()
+ * and checks whether the expected characters are present.
+ * Must be run after testFPRINTF().
+ */
+void testFREAD(void)
+{
+   unsigned char buffer[20];
+
+   if (NULL != temp_file) {
+      rewind(temp_file);
+      CU_ASSERT(9 == fread(buffer, sizeof(unsigned char), 20, temp_file));
+      CU_ASSERT(0 == strncmp(buffer, "Q\ni1 = 10", 9));
+   }
+}
+
+/* The main() function for setting up and running the tests.
+ * Returns a CUE_SUCCESS on successful running, another
+ * CUnit error code on failure.
+ */
+int main()
+{
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if ((NULL == CU_add_test(pSuite, "test of fprintf()", testFPRINTF)) ||
+       (NULL == CU_add_test(pSuite, "test of fread()", testFREAD)))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
+}
